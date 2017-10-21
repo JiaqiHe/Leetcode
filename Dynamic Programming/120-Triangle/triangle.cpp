b509d2bff24adcc08f5dbdc7007993481d130b9b
Jiaqi He <hjq53589793@outlook.com>
2017-10-21 12:40:07 -0700

    Create triangle.cpp

---
+++ Dynamic Programming/120-Triangle/triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        if(triangle.empty() || triangle[0].empty()) return 0;
+        vector<int> res(triangle.back());
+        for(int i = triangle.size()-2; i >=0; i --){
+            for(int j = 0; j <= i; j ++){
+                res[j] = triangle[i][j] + min(res[j], res[j+1]);
+            }
+        }
+        return res[0];
+    }
+};
