369d8ebdba0c7a17134e972bc0b343bc0d9fefdd
Jiaqi He <hjq53589793@outlook.com>
2017-11-03 18:17:17 -0700

    Create wildcard_matching.cpp

---
+++ Dynamic Programming/44-Wildcard Matching/wildcard_matching.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(p.empty()) return s.empty();
+        int len_s = s.length(), len_p = p.length();
+        bool dp[len_s+1][len_p+1];
+        
+        dp[0][0] = true;
+        for(int i = 1; i <= len_s; i ++){
+            dp[i][0] = false;
+        }
+        for(int i = 1; i <= len_p; i ++){
+            dp[0][i] = p[i-1]=='*'&&dp[0][i-1];
+        }
+        for(int i = 1; i <= len_s; i ++){
+            for(int j = 1; j <= len_p; j ++){
+                if(p[j-1] != '*'){
+                    dp[i][j] = dp[i-1][j-1] && (s[i-1] == p[j-1] || p[j-1] == '?');
+                }
+                else{
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
+                }
+            }
+        }
+        return dp[len_s][len_p];
+    }
+};
