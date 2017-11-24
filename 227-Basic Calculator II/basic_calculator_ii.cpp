5a4299099c968d62efe61685556e3b41c545d9ff
Jiaqi He <hjq53589793@outlook.com>
2017-11-24 15:53:43 -0800

    Update basic_calculator_ii.cpp

--- 227-Basic Calculator II/basic_calculator_ii.cpp
+++ 227-Basic Calculator II/basic_calculator_ii.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int calculate(string s) {
+        s += ')';
         int res = 0;
         stack<int> nums, ops;
+        ops.push(1);
         int num = 0;
         for(char c : s){
             if(c == ' ') continue;
@@ -29,24 +31,11 @@ public:
                 if(c == '/') ops.push(4);
             }
         }
-        if(!ops.empty() && (ops.top() == 3 || ops.top() == 4)){
-            int pre = nums.top();
-            nums.pop();
-            int op = ops.top();
-            ops.pop();
-            if(op == 3) nums.push(pre*num);
-            if(op == 4) nums.push(pre/num);
-            num = 0;
-        }
-        else{
-            nums.push(num);
-            num = 0;
-        }
         while(!ops.empty()){
             res += nums.top() * ops.top();
             nums.pop();
             ops.pop();
         }
-        return res + nums.top();
+        return res;
     }
 };
