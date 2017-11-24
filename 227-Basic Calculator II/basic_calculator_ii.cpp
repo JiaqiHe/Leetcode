3cd16fcfcec2e9ce6ca1493797bf6fc465aca7e9
Jiaqi He <hjq53589793@outlook.com>
2017-11-24 15:51:00 -0800

    Create basic_calculator_ii.cpp

---
+++ 227-Basic Calculator II/basic_calculator_ii.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int calculate(string s) {
+        int res = 0;
+        stack<int> nums, ops;
+        int num = 0;
+        for(char c : s){
+            if(c == ' ') continue;
+            if(isdigit(c)){
+                num = num * 10 + c - '0';
+            }
+            else{
+                if(!ops.empty() && (ops.top() == 3 || ops.top() == 4)){
+                    int pre = nums.top();
+                    nums.pop();
+                    int op = ops.top();
+                    ops.pop();
+                    if(op == 3) nums.push(pre*num);
+                    if(op == 4) nums.push(pre/num);
+                    num = 0;
+                }
+                else{
+                    nums.push(num);
+                    num = 0;
+                }
+                if(c == '+') ops.push(1);
+                if(c == '-') ops.push(-1);
+                if(c == '*') ops.push(3);
+                if(c == '/') ops.push(4);
+            }
+        }
+        if(!ops.empty() && (ops.top() == 3 || ops.top() == 4)){
+            int pre = nums.top();
+            nums.pop();
+            int op = ops.top();
+            ops.pop();
+            if(op == 3) nums.push(pre*num);
+            if(op == 4) nums.push(pre/num);
+            num = 0;
+        }
+        else{
+            nums.push(num);
+            num = 0;
+        }
+        while(!ops.empty()){
+            res += nums.top() * ops.top();
+            nums.pop();
+            ops.pop();
+        }
+        return res + nums.top();
+    }
+};
