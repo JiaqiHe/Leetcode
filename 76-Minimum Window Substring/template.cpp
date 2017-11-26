01346b65a0a8d137658efdded80f0a57685705bb
Jiaqi He <hjq53589793@outlook.com>
2017-11-25 18:07:45 -0800

    Update template.cpp

--- 76-Minimum Window Substring/template.cpp
+++ 76-Minimum Window Substring/template.cpp
@@ -23,3 +23,7 @@ int findSubstring(string s){
         }
         return d;
   }
+
+
+//https://discuss.leetcode.com/topic/30941/here-is-a-10-line-template-that-can-solve-most-substring-problems
+//credit: zjh08177 
